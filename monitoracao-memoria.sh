77c93f6d00e2c0ee028f1d4ef4e9b0c9a6d7d8a9
Kbesa <106637227+HeinrickCamargos@users.noreply.github.com>
2024-03-04 11:53:46 -0300

    Update monitoracao-memoria.sh

--- monitoracao-memoria.sh
+++ monitoracao-memoria.sh
@@ -7,12 +7,16 @@
 #
 #########################################
 
+#
+# OBS: esse Script vai monitorar a memoria do servidor e verificar se ela está consumindo demais, caso sim ela ira enviar um email para a conta cadastrada no script. 
+#
+
 memoria_total=$(free | grep -i mem | awk '{ print $2 }')
 memoria_consumida=$(free | grep -i mem | awk '{ print$3  }')
 relacao_memoria_atual_total=$(bc <<< "scale=2;memoria_consumida/$memoria_total *100" | awk -F. '{ print $1 }')
 
 if [ $relacao_memoria_atual_total -gt 50 ]
 then
-
-        echo "Consumo de memoria acima do limite" | msmtp -a gmail heinrickcomunix@gmail.com
+# Neste trecho que será enviado o email.
+        echo "Consumo de memoria acima do limite" | msmtp -a gmail heinrickcomunix@gmail.com 
 fi
