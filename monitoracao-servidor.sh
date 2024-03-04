7a6d3ab0b54e2c43e1406f4c8a0aabbcc6ff54ca
Kbesa <106637227+HeinrickCamargos@users.noreply.github.com>
2024-03-04 11:56:33 -0300

    Update monitoracao-servidor.sh

--- monitoracao-servidor.sh
+++ monitoracao-servidor.sh
@@ -7,10 +7,15 @@
 #
 #########################################
 
+#
+# OBS: esse Script tem a função de restart o servidor de apache caso o mesmo esteja fora, porem ele vai disparar um email para o ADM, o sinalizando que o servidor tinha caido. Lembrando que ele so vai ser acionado pelo crontab
+#
+
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://192.168.9.35)
 
 if [ $resposta_http -ne 200 ]
 then
+# Neste trecho que será enviado o email.
         echo "Houve um problema no servidor e os usuários pararam de ter acesso ao conteúdo, favor verificar." | msmtp -a gmail heinrickcomunix@gmail.com
         systemctl restart apache2
 fi
